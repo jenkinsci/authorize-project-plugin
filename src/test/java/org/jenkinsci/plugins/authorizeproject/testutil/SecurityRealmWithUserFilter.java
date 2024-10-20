558856fe71b48318a85bd4f6dcc1b0fc06585b60
Mark Waite <mark.earl.waite@gmail.com>
2024-10-20 16:07:15 -0600

    Use Java 17 language features
    Formatted strings

--- src/test/java/org/jenkinsci/plugins/authorizeproject/testutil/SecurityRealmWithUserFilter.java
+++ src/test/java/org/jenkinsci/plugins/authorizeproject/testutil/SecurityRealmWithUserFilter.java
@@ -51,8 +51,7 @@ public class SecurityRealmWithUserFilter extends SecurityRealm {
                 baseComponent.manager,
                 username -> {
                     if (!validUserList.contains(username)) {
-                        throw new UsernameNotFoundException(
-                                String.format("%s is not listed as valid username.", username));
+                        throw new UsernameNotFoundException("%s is not listed as valid username.".formatted(username));
                     }
                     return baseComponent.userDetails.loadUserByUsername(username);
                 },
